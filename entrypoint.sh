531821582911463fd1ce54135d473df60938ab90
AktanKasymaliev <aktan.kasymaliev@icloud.com>
2022-06-27 11:47:12 +0600

    edited entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 python manage.py makemigrations --no-input
+python manage.py makemigrations gamble --no-input
 python manage.py migrate --no-input
+python manage.py migrate gamble --no-input
 python manage.py collectstatic --no-input
 python manage.py createsuperuser
 
